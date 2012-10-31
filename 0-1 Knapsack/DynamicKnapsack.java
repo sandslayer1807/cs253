4bc3d170d9c84bf62119fd78693a51f5ad3ba3ea
Ethan <colledgekidz@gmail.com>
2012-10-31 04:40:50 -0500

    Got everything working and added asserts. Need to work on greedy algorithm more, but that is better left to a different date.

--- 0-1 Knapsack/DynamicKnapsack.java
+++ 0-1 Knapsack/DynamicKnapsack.java
@@ -30,23 +30,27 @@ public class DynamicKnapsack {
     public static void main (String[] args)
     {  
         //number of times to run:
-        int numTimesToRun = 5;
+        int numTimesToRun = 1;
         
         // For loop to control how many times to run each algorithm for certain
         //  values of n.
         
-        for(int k = 5; k <= 30; k++)                            
+        for(int k = 2; k <= 17; k++)                            
         {   
             //Create the profit and weight arrays.
             int[] profitArray = new int[k];
             int[] weightArray = new int[k]; 
+            
             //Create the capacity of the knapsack to be 2^n.
             int capacityOfKnapsack = (int)(Math.exp(k));
+            
             //Initialize the value that the algorithms compare their runtime against
             long finalTime = Long.MAX_VALUE;
+            
             //Fill the profit and weight arrays.
             generateArray(capacityOfKnapsack, weightArray);
             generateArray(capacityOfKnapsack, profitArray);
+            
             //Structure to help control fluctuating runtime; runs algorithm a few
             //  times per n-value to reduce interference from other processes.
             for(int i = numTimesToRun; i > 0; i--)
@@ -54,16 +58,19 @@ public class DynamicKnapsack {
                 //start timing
                 long beginTime = System.nanoTime();
                 //Start algorithm
-                //greedyKnapsack(weightArray,profitArray,capacityOfKnapsack);
-                recursiveKnapsack(weightArray,profitArray,0,capacityOfKnapsack,0);
+                int greed = greedyKnapsack(weightArray,profitArray,capacityOfKnapsack);
+                int recurse = recursiveKnapsack(weightArray,profitArray,0,capacityOfKnapsack);
+                int iterative = iterativeKnapsack(weightArray,profitArray,capacityOfKnapsack);
                 //Stop timing
                 long endTime = System.nanoTime() - beginTime;
                 if(endTime < finalTime && endTime != 0)
                 {
                     finalTime = endTime;
                 }
+                //Print out results
+                System.out.println(k+" "+recurse+" "+iterative+" "+greed);
             }
-        System.out.println("Ran algo. for "+k+" inputs with a time of "+finalTime);
+        //System.out.println(k+" "+finalTime);
         }
     }
     
@@ -75,46 +82,106 @@ public class DynamicKnapsack {
     // Precondition: Given arrays containing weights and profits and the capacity of the 
     //              knapsack. The arrays containing weights and profits must be the same size
     //              as the number of items to select from, and the weight array must contain weights > 0.
+    //              The arrays must also be valid arrays.
     // Postcondition: Returns the maximum profit possible from the item choices/knapsack
-    //              capacity given, using the memoized solution from dynamic programming.
+    //              capacity given, using the memoized solution from dynamic programming. This
+    //              profit is less than or equal to the optimal profit from the recursive algorithm.
     // Return: An integer representing the maximum profit possible.
     ///////////////////////////////////////////////////////////////////////////
     public static int iterativeKnapsack( final int[] weightArray, final int[] profitArray, int capacityOfKnapsack)
     {
-        
-        // Declaring the solutions array and the 'traceback' array, or the array of items kept in the knapsack.
-        int [][] solArray = new int[weightArray.length][capacityOfKnapsack];
-        int [][] keepersArray = new int[weightArray.length][capacityOfKnapsack];
-        // Fill the top row with zeroes as we don't terribly care about them.
-        for( int k = 0; k < capacityOfKnapsack; k++)
+        //Asserting the precondition
+        for( int j = 0; j < weightArray.length; j++)
         {
-            solArray[0][k] = 0;
+            assert( weightArray[j] > 0);
         }
-        //While we still have items to fit
-        for( int k = 0; k < weightArray.length; k++)
+        //Declaring solutions array, final sum, and keeper array.
+        int [][] solArray = new int[weightArray.length+1][capacityOfKnapsack+1];
+        int sumOfProfits = 0;
+        int[] itemsInKnapsack = new int[weightArray.length+1];
+        
+        //Shifting arrays to start at 1 to match book's pseudocode.
+        int[] weight = new int[weightArray.length+1];
+        System.arraycopy( weightArray, 0, weight, 1, weightArray.length);
+        int[] profit = new int[profitArray.length+1];
+        System.arraycopy( profitArray, 0, profit, 1, profitArray.length);
+        
+        //Put initial zeroes in the table of solutions.
+        for(int k = 0; k < weight[weightArray.length]; k++)
         {
-            //While the knapsack still has room
-            for( int m = 1; m < capacityOfKnapsack; m++ )
+            solArray[weightArray.length][k] = 0;
+        }
+        for( int m = weight[weightArray.length]; m <= capacityOfKnapsack; m++)
+        {
+            solArray[profitArray.length][m] = profit[profitArray.length];
+        }
+        
+        //Starting the computation of the tuples in the table.
+        for( int k = weightArray.length-1; k > 1; k--)
+        {
+            for( int j = 0; j < weight[k]; j++)
             {
-                // If the item weighs less than the capacity of the knapsack AND picking up the item is better than not picking it up
-                if( weightArray[k] <= m && (profitArray[k] + solArray[k - 1][(m - weightArray[k])] > solArray[k - 1][m] ) )
-                {
-                    // Pick up the item
-                    solArray[k][m] = profitArray[k] + solArray[k - 1][m - weightArray[k]];
-                    //Add it to the "keepers" array
-                    keepersArray[k][m] = 1;
-                }
-                // If we don't pick up the item
-                else
-                {
-                    // Don't pick up the item and move on (without passing GO and without collecting $200)
-                    solArray[k][m] = solArray[k - 1][m];
-                    keepersArray[k][m] = 0;
-                }
+                //Value equals the value above it.
+                solArray[k][j] = solArray[k+1][j];
+            }
+            for( int j = weight[k]; j <= capacityOfKnapsack; j++)
+            {
+                //Value equals the max of the value directly above it and the value if you had taken the item
+                solArray[k][j] = max( solArray[k+1][j], solArray[k+1][j-weight[k]] + profit[k]);
             }
         }
-        //Return the highest profit, located at the bottom left of the array.
-        return solArray[weightArray.length-1][capacityOfKnapsack-1];   
+        solArray[1][capacityOfKnapsack] = solArray[2][capacityOfKnapsack];
+        //If you have room to take the first item
+        if( capacityOfKnapsack >= weightArray[1])
+        {
+            //Figure out if it is the best option
+            solArray[1][capacityOfKnapsack] = max( solArray[1][capacityOfKnapsack], solArray[2][capacityOfKnapsack - weight[1]] + profit[1]);
+        }
+        
+        //Figure out which items we picked to calculate the profit
+        for( int j = 1; j < weightArray.length; j++)
+        {
+            //If the rightmost jth item equals the item above it (initial run)
+            //else, it is the (capacity-weights of items picked)th, jth item
+            if(solArray[j][capacityOfKnapsack] == solArray[j+1][capacityOfKnapsack])
+            {
+                //Didn't pick the item
+                itemsInKnapsack[j] = 0;
+            }
+            else //picked the item
+            {
+                //subtract weight and "pick" the item
+                itemsInKnapsack[j] = 1;
+                capacityOfKnapsack -= weight[j];
+            }
+        }
+        
+        //Last item handling
+        if( solArray[weightArray.length][capacityOfKnapsack] == 0)
+        {
+            //Didn't pick it.
+            itemsInKnapsack[weightArray.length] = 0;
+        }
+        else
+        {
+            //Picked it.
+            itemsInKnapsack[weightArray.length] = 1;
+        }
+        
+        //Figuring out dem profits...
+        for(int j = 0; j <= weightArray.length; j++)
+        {
+            //If item was picked
+            if( itemsInKnapsack[j] == 1)
+            {
+                //Add profit of said item
+                sumOfProfits += profit[j];
+            }
+        }
+        //Return dem profits.
+        //Asserting the postcondtion: This algorithm returns profits that are less than or
+        //      equal to the exact profits reported by the recursive algorithm.
+        return sumOfProfits;
     }
  
     ///////////////////////////////////////////////////////////////////////////
@@ -128,19 +195,23 @@ public class DynamicKnapsack {
     //              number of items as the number of items we are looking to choose from.
     // Postcondition: Returns the maximum profit possible from the item choices/knapsack
     //              capacity given, using the greedy method (picking the most profitable
-    //              items first).
+    //              items first). The profit returned by this algorithm must be less than or equal
+    //              to the solution reported by the iterative algorithm (optimal solution).
     // Return: An integer representing the maximum profit possible from the greedy
     //          method.
     ///////////////////////////////////////////////////////////////////////////    
     public static int greedyKnapsack( final int[] weightArray, final int[] profitArray, int knapsackCapacity)
     {
+        //Asserting the precondition
+        for( int j = 0; j < weightArray.length; j++)
+        {
+            assert( weightArray[j] > 0);
+        }
         // Sort the array based on weight-to-profit, and start picking up items
         int[] profitWeight = new int[weightArray.length];
         // Traceback array to figure out what items need to be put into the knapsack
         int[] traceBackArray = new int[weightArray.length];
-        // Solution array
-        //int[] solutionArray = new int[weightArray.length];
-        //int solutionArrayIndex = 0;
+
         int finalProfit = 0;
         // For the items in the profit/weight array
         for( int i = 0; i < profitWeight.length; i++)
@@ -149,23 +220,22 @@ public class DynamicKnapsack {
             profitWeight[i] = (int)(profitArray[i] / weightArray[i]);
             traceBackArray[i] = profitWeight[i];
         }   
-        //Sort the array with insertion sort
-        profitWeight = algorithms.SortingAlgorithms.insertionSort( profitWeight );
+        //Sort the array with insertion sort; insertion sort is used due to efficiency.
+        System.arraycopy(profitWeight,0,algorithms.SortingAlgorithms.insertionSort(profitWeight),0,profitWeight.length);
         for( int i = 0; i < profitWeight.length; i++ )
         {
             //If the knapsack has room for the item
-            int indexOfObject = findItem(profitArray, traceBackArray, i);
+            int indexOfObject = findItem(profitWeight, traceBackArray, i);
             if( weightArray[indexOfObject] <= knapsackCapacity )
             {
-                //Put the item into the knapsack
-                // solutionArray[solutionArrayIndex] = indexOfObject;
-                // solutionArrayIndex++;
+                //Calculate profit
                 finalProfit += profitArray[indexOfObject];
+                knapsackCapacity -= weightArray[indexOfObject];
             }
         }
-        //Putting in the delimiting item of the solution array
-        // solutionArray[solutionArrayIndex] = -1;
         //Returning the maximum profit.
+        //Postcondition: the profit reported by this algorithm must be less than or equal to the profit
+        //      reported by the iterative algorithm (optimal solution).
         return finalProfit;       
     }
 
@@ -183,34 +253,26 @@ public class DynamicKnapsack {
     //              capacity given, via the recursive (optimal) solution.
     // Return: An integer representing the maximum profit possible.
     ///////////////////////////////////////////////////////////////////////////
-    public static int recursiveKnapsack( final int[] weightArray, final int profitArray[], int index, int knapsackCapacity, int profitFound)
+    public static int recursiveKnapsack( final int[] weightArray, final int profitArray[], int index, int knapsackCapacity)
     {
+        //Asserting the precondition
+        for( int j = 0; j < weightArray.length; j++)
+        {
+            assert( weightArray[j] > 0);
+        }
         //Stopping case: last item in the array
-        if( (weightArray.length - index) == 1 )
+        if( index == weightArray.length-1 )
         {
             //if the item fits, take the item and get the profit. if not, then don't.
-            return weightArray[index] <= knapsackCapacity ? profitFound+profitArray[index] : profitFound;
+            return knapsackCapacity < weightArray[index] ? 0 : profitArray[index];
         }
         //If not at the last item in the array, keep going
-        else
+        if( knapsackCapacity < weightArray[index])
         {
-            //If the item fits... (it ships) [ reference to UPS flat rate "knapsacks" ] 
-            if( weightArray[index] <= knapsackCapacity)
-            {
-                //int to store value of not taking item
-                int leaveItem = recursiveKnapsack( weightArray, profitArray, index+1, knapsackCapacity, profitFound);
-                //int to store value of taking item
-                int takeItem = recursiveKnapsack( weightArray, profitArray, index+1, knapsackCapacity - weightArray[index], profitFound + profitArray[index]);
-                //Compare the value of taking the item and leaving the item, and pick the greatest of those
-                return max(leaveItem, takeItem);
-            }
-            //the item doesn't fit
-            else
-            {
-                //don't pick the item and move on
-                return recursiveKnapsack ( weightArray, profitArray, index+1, knapsackCapacity, profitFound);
-            }
+            return recursiveKnapsack( weightArray, profitArray, index+1, knapsackCapacity);
         }
+        //Asserting the postcondition: The profit returned from this algorithm is the combination of the profits calculated from the subarrays and is the optimal solution.
+        return max( recursiveKnapsack(weightArray, profitArray, index+1, knapsackCapacity), (recursiveKnapsack(weightArray, profitArray, index+1, knapsackCapacity - weightArray[index]) + profitArray[index]));
     }
     
     ///////////////////////////////////////////////////////////////////////////
@@ -250,9 +312,10 @@ public class DynamicKnapsack {
     public static int findItem ( final int[] ratioArray, final int[] sourceArray, int indexToLook)
     {
         //Check to see if the index is valid. (Both arrays are same size)
-        if( indexToLook <= ratioArray.length)
+        if( indexToLook >= ratioArray.length)
         {
-            return 0;
+            //ERROR!?!
+            return -1;
         }
         //While there are items to search in the source array
         for( int i = 0; i < sourceArray.length; i++)
@@ -267,15 +330,24 @@ public class DynamicKnapsack {
         //Can't find the item
         return 0;
     }
-    
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Method: public static int max( final int a, final int b)
+    // Description: A max function to determine the max of the two items passed in
+    // Precondition: a and b are valid integers.
+    // Postcondition: Returns the value that is greater.
+    // Return: ^^^^
+    ///////////////////////////////////////////////////////////////////////////
     public static int max (final int a, final int b)
     {
-        return a >= b ? a : b;
+        if( a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
     }
-    
-    public static void test ()
-    {
         
-    }
-    
 }
